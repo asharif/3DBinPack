0d0e2397bee55e3c0fa43600f70faa8277a0ffdf
Arash Sharif <asharif.mse@gmail.com>
2012-04-30 16:26:48 -0700

    added ability for diff side tol+kerf for java and c

--- c/pack3d.c
+++ c/pack3d.c
@@ -34,7 +34,7 @@ main(int argc, char * argv[])
 
 	if ( argc < 4 )
         {
-                printf("3 arguments required: bin size (eg. 5x5x5), box size (eg. 1x1x1) and tol+kerf (eg 1.25)\n");
+                printf("3 arguments required: bin size (eg. 5x5x5), box size (eg. 1x1x1) and tol+kerf for each side(eg 0.25,0.25,0.25)\n");
                 exit(1);
 
         }
@@ -42,11 +42,11 @@ main(int argc, char * argv[])
 
 	float binSizeV[3];
 	float boxSizeV[3];
+	float tolKerfV[3];
 	
 	getSizeVector(argv[1], binSizeV, "x");
 	getSizeVector(argv[2], boxSizeV, "x");	
-
-	PACK3D_tolKerf = atof(argv[3]);
+	getSizeVector(argv[3], tolKerfV, ",");
 	
 
 	struct PACK3D_shape *bin;
@@ -56,15 +56,24 @@ main(int argc, char * argv[])
 
 
 
-	bin->w = binSizeV[0];
-	bin->h = binSizeV[1];
-	bin->l = binSizeV[2];
+	bin->w = ( struct PACK3D_side * ) malloc(sizeof( struct PACK3D_side ));
+	bin->w->size = binSizeV[0];
+	bin->w->tk = tolKerfV[0];
+	bin->h = ( struct PACK3D_side * ) malloc(sizeof( struct PACK3D_side ));
+	bin->h->size = binSizeV[1];
+	bin->h->tk = tolKerfV[1];
+	bin->l = ( struct PACK3D_side * ) malloc(sizeof( struct PACK3D_side ));
+	bin->l->size = binSizeV[2];
+	bin->l->tk = tolKerfV[2];
 	bin->d = NULL;
 	bin->r = NULL;
 	bin->b = NULL;
-	box->w = boxSizeV[0];
-	box->h = boxSizeV[1];
-	box->l = boxSizeV[2];
+	box->w = ( struct PACK3D_side * ) malloc(sizeof( struct PACK3D_side ));
+	box->w->size = boxSizeV[0];
+	box->h = ( struct PACK3D_side * ) malloc(sizeof( struct PACK3D_side ));
+	box->h->size = boxSizeV[1];
+	box->l = ( struct PACK3D_side * ) malloc(sizeof( struct PACK3D_side ));
+	box->l->size = boxSizeV[2];
 
 	//struct timeval start;
 	//struct timeval end;
@@ -74,7 +83,13 @@ main(int argc, char * argv[])
 	PACK3D_packIt(bin, box);
 
 	//free them!
+	free(bin->w);
+	free(bin->h);
+	free(bin->l);
 	free(bin);
+	free(box->w);
+	free(box->h);
+	free(box->l);
 	free(box);
 
 	//gettimeofday(&end);
